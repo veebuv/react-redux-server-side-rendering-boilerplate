5aa96a6215e413c1363ee28e61c25ea41624af2f
veebuv <veebuv@gmail.com>
2016-08-14 03:12:26 +1000

    css module

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,36 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://0.0.0.0:8080',
     'webpack/hot/only-dev-server',
-    './src/index.js'
+    './src/index.js',
   ],
   output: {
     path: __dirname,
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
 
   externals: {
-    'cheerio': 'window',
+    cheerio: 'window',
     'react/addons': true,
     'react/lib/ExecutionEnvironment': true,
     'react/lib/ReactContext': true,
-  }
+  },
   module: {
-    loaders: [{test: /\.js$/, loaders: ['react-hot','babel'], exclude: /node_modules/,},
-    ]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'], exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style?sourceMap',
+          'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+        ],
+      },
+    ],
   },
 };
